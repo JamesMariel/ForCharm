2a9267b8b0a14d597c6d2d89ba5fa191c16b7b1a
JamesMariel <77163282+JamesMariel@users.noreply.github.com>
2022-03-09 03:12:43 +0630

    Update index.js

--- Wish-Her/message/js/index.js
+++ Wish-Her/message/js/index.js
@@ -12,7 +12,7 @@ function animateText(){
 		span.innerHTML = item;
 		animatedSpan.push(span);
 		span.style.opacity = 0;
-		span.style.color = 'white';
+		span.style.color = 'pink';
 		this.$dom.animatedText.appendChild(span);
 	});
 	TweenMax.staggerTo(animatedSpan, .2,{
